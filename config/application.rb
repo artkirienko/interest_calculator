3e88696de5ada59b10b78a3f1ddf3cb4628277ee
Artemy Kirienko <art-kirienko@ya.ru>
2017-08-20 23:35:42 +0300

    Use russian locale, currency fix :rose:

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,9 @@ module InterestCalculator
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.time_zone = 'Moscow'
+    config.i18n.available_locales = [:en, :ru]
+    config.i18n.default_locale = :ru
+    config.i18n.fallbacks = [:en]
   end
 end
