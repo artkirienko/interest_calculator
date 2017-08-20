a1abbffe8afb3c3acde5bd740542450c7c75acd9
Artemy Kirienko <art-kirienko@ya.ru>
2017-08-20 22:35:42 +0300

    Add client calculation methods :bread:

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,16 @@
 class Client < ApplicationRecord
   has_many :loans
   validates :name, presence: true
+
+  def annual_realistic
+    payed_interest = loans.inject(0.0) {|sum, loan| sum += loan.payed_interest}
+    payed_base = loans.inject(0.0) {|sum, loan| sum += loan.payed_base}
+    1.0 * payed_interest / payed_base / loans.first.tariff.term * 12
+  end
+
+  def annual_optimistic
+    annual_interest = loans.inject(0.0) {|sum, loan| sum += (loan.monthly_interest * loan.tariff.term)}
+    annual_base = loans.inject(0.0) {|sum, loan| sum += (loan.monthly_base * loan.tariff.term)}
+    1.0 * (annual_interest) / (annual_base) / loans.first.tariff.term * 12
+  end
 end
