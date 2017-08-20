5a15f63e0e0755d02d439621db88fe941f87e57a
Artemy Kirienko <art-kirienko@ya.ru>
2017-08-20 22:00:51 +0300

    Fix navbar :hammer:

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def nav_bar
+    content_tag(:ul, class: "nav navbar-nav") do
+      yield
+    end
+  end
+
+  def nav_link(text, path)
+    options = (controller.controller_name == path.sub('/', '')) ? { class: "active" } : {}
+    content_tag(:li, options) do
+      link_to text, path
+    end
+  end
 end
