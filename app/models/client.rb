47df121c1c8d0dc97e61e8a406cb03a77697ab10
Artemy Kirienko <art-kirienko@ya.ru>
2017-08-21 22:56:21 +0300

    Round final values inline :evergreen_tree:

--- app/models/client.rb
+++ app/models/client.rb
@@ -6,12 +6,12 @@ class Client < ApplicationRecord
   def annual_realistic
     payed_interest = loans.inject(0.0) {|sum, loan| sum += loan.payed_interest}
     payed_base = loans.inject(0.0) {|sum, loan| sum += loan.payed_base}
-    1.0 * payed_interest / payed_base / loans.first.tariff.term * 12
+    (1.0 * payed_interest / payed_base / loans.first.tariff.term * 12).round(4)
   end
 
   def annual_optimistic
     annual_interest = loans.inject(0.0) {|sum, loan| sum += (loan.monthly_interest * loan.tariff.term)}
     annual_base = loans.inject(0.0) {|sum, loan| sum += (loan.monthly_base * loan.tariff.term)}
-    1.0 * (annual_interest) / (annual_base) / loans.first.tariff.term * 12
+    (1.0 * annual_interest / annual_base / loans.first.tariff.term * 12).round(4)
   end
 end
